d31a4d1f3c316b75d02598a553a3e6fbad452e88
thalia244-code <thalia.santos.araujo@escola.pr.gov.br>
2025-08-11 09:07:25 -0300

    YGT6T

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'planeta',
+    'qual é o maior planta o sistma solar?',
+    'Júpiter'
+)
+criaCartao(
+    'BrasI',
+    'em que ano Brasi foi descoberto pelos poruguses?',
+    'em 1500 '
+)
+criaCartao(
+    'Programação',
+    'quem escreveu Dom Casmurro ?',
+    'Machado de Assis
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
